6e29e1e5c56dfa08b1b7e913ff42904f5f5e28ce
Artyom Burcha <artem.burcha@gmail.com>
2018-09-30 22:43:16 +0300

    табличный метод получения мат. моделей систем

---
+++ trudTablMet/tabl/cmake-build-debug/CMakeFiles/tabl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tabl.dir/main.c.o"
+  "tabl.pdb"
+  "tabl"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tabl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
